09ff3feea8b319fd795e4af5c98e66531effbc4a
Mesa123456789 <masapiamonpat@gmail.com>
2026-05-18 16:44:55 +0700

    hdrp to urp

---
+++ Assets/Script/Light/SkyManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SkyManager : MonoBehaviour
+{
+    public float skySpeed;  // ✅ ย้ายมาไว้ระดับ class
+
+    void Start()
+    {
+        // ไม่มีอะไรต้องทำใน Start ตอนนี้
+    }
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * skySpeed);
+    }
+}
